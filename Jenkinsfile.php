5ebcf83064a0a9f95c0fb0037f2bfde2d3fb4750
mangeldo <79280333+mangeldo@users.noreply.github.com>
2021-02-18 23:45:30 +0100

    Add files via upload

---
+++ Jenkinsfile.php
@@ -0,0 +1,33 @@
+import groovy.json.JsonSlurperClassic
+
+def jsonParse(def json) {
+  new groovy.json.JsonSluperClassic().parseText(json)
+}
+pipeline {
+agent { label 'master' }
+environment {
+ appName = "variable"
+}
+stage("paso 1"){
+
+    steps {
+       script {
+        sh "echo 'hola mundo'"
+      }
+     }
+    }
+  }
+ 
+post {
+    always {
+       deleteDir()
+        sh "echo 'fase always'"
+       }
+     success {
+        sh "echo 'fase success' "
+      }
+     failture {
+            sh "echo 'fase failure'"
+      }
+  }
+}
